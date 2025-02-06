6fe30ac70695beefa8053d778282eb66e537912d
Pierre <pierre.provost@orange.fr>
2025-02-06 18:38:25 +0100

    grid working, onclick and view

--- tp1/lib/main.dart
+++ tp1/lib/main.dart
@@ -9,45 +9,18 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 31, 185, 38)),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: false,
+      home: const MyHomePage(title: 'Encyclopédie Dofus'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -58,11 +31,7 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   int _selectedIndex = 0;
 
-  static const List<Widget> _pages = <Widget>[
-    Page1(),
-    Page2(),
-    Page3(),
-  ];
+  static const List<Widget> _pages = <Widget>[Page1(), Page2(), Page3()];
 
   void _onItemTapped(int index) {
     setState(() {
@@ -73,25 +42,21 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      extendBodyBehindAppBar: true,
       appBar: AppBar(
         title: Text(widget.title),
+
+        backgroundColor: Color.fromARGB(255, 210, 255, 195),
+        scrolledUnderElevation: 0.0,
       ),
-      body: Center(
-        child: _pages.elementAt(_selectedIndex),
-      ),
+      body: _pages.elementAt(_selectedIndex),
       bottomNavigationBar: BottomNavigationBar(
         items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Page 1'),
+          BottomNavigationBarItem(icon: Icon(Icons.business), label: 'Page 2'),
           BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Page 1',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.business),
-            label: 'Page 2',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.school),
-            label: 'Page 3',
+            icon: Icon(Icons.alt_route),
+            label: 'Challenges',
           ),
         ],
         currentIndex: _selectedIndex,
@@ -107,9 +72,7 @@ class Page1 extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Page 1'),
-    );
+    return const Center(child: Text('Page 1'));
   }
 }
 
@@ -118,9 +81,7 @@ class Page2 extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Page 2'),
-    );
+    return const Center(child: Text('Page 2'));
   }
 }
 
@@ -133,119 +94,174 @@ class Page3 extends StatefulWidget {
 
 class _Page3State extends State<Page3> {
   late Future<List<dynamic>> _data;
-  int _challengeCount = 5;
-  List<bool> _expandedTiles = [];
 
   @override
   void initState() {
     super.initState();
-    _data = fetchData(_challengeCount);
+    _data = fetchData();
   }
 
-  Future<List<dynamic>> fetchData(int count) async {
-    final response = await http.get(Uri.parse('https://api.dofusdb.fr/challenges?\$skip=0&\$sort[slug.fr]=1&\$limit=50&categoryId[]=1&iconId[\$ne]=0&lang=fr'));
+  Future<List<dynamic>> fetchData() async {
+    final response = await http.get(
+      Uri.parse(
+        'https://api.dofusdb.fr/challenges?\$skip=0&\$sort[slug.fr]=1&\$limit=50&categoryId[]=1&iconId[\$ne]=0&lang=fr',
+      ),
+    );
 
     if (response.statusCode == 200) {
       final data = json.decode(response.body);
-      _expandedTiles = List<bool>.filled(data['data'].length, false);
-      return data['data'].take(count).toList();
+      return data['data'].toList();
     } else {
       throw Exception('Failed to load data');
     }
   }
 
-  void _updateChallengeCount(double value) {
-    setState(() {
-      _challengeCount = value.toInt();
-      _data = fetchData(_challengeCount);
-    });
-  }
-
-  void _toggleTileExpansion(int index) {
-    setState(() {
-      _expandedTiles[index] = !_expandedTiles[index];
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Challenges'),
-      ),
       body: Column(
         children: [
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Row(
-              children: [
-                Expanded(
-                  child: Slider(
-                    value: _challengeCount.toDouble(),
-                    min: 1,
-                    max: 39,
-                    divisions: 38,
-                    label: _challengeCount.toString(),
-                    onChanged: _updateChallengeCount,
-                  ),
-                ),
-                Text('$_challengeCount challenges'),
-              ],
-            ),
-          ),
           Expanded(
-            child: FutureBuilder<List<dynamic>>(
+            child: FutureBuilder(
               future: _data,
               builder: (context, snapshot) {
                 if (snapshot.connectionState == ConnectionState.waiting) {
-                  return const CircularProgressIndicator();
-                } else if (snapshot.hasError) {
-                  return Text('Error: ${snapshot.error}');
-                } else {
-                  final challenges = snapshot.data!;
                   return Padding(
                     padding: const EdgeInsets.all(8.0),
                     child: GridView.builder(
-                      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                        crossAxisCount: 4, // Nombre de colonnes dans la grille
+                      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+                        maxCrossAxisExtent: 150,
                         crossAxisSpacing: 8.0,
                         mainAxisSpacing: 8.0,
                       ),
-                      itemCount: challenges.length,
+                      itemCount: 4,
                       itemBuilder: (context, index) {
-                        final challenge = challenges[index];
-                        final isExpanded = _expandedTiles[index];
-                        return GestureDetector(
-                          onTap: () => _toggleTileExpansion(index),
-                          child: AnimatedContainer(
-                            duration: const Duration(milliseconds: 300),
-                            curve: Curves.easeInOut,
-                            height: isExpanded ? 200 : 100, // Ajustez la hauteur en fonction de l'état
-                            child: Card(
-                              child: Padding(
-                                padding: const EdgeInsets.all(8.0),
-                                child: Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    Image.network(challenge['img']),
-                                    Text('Name: ${challenge['name']['fr']}'),
-                                    if (isExpanded) ...[
-                                      Text('Description: ${challenge['description']['fr']}'),
-                                    ],
-                                  ],
-                                ),
-                              ),
+                        return Card(
+                          color: const Color.fromARGB(255, 153, 209, 92),
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: CircularProgressIndicator(
+                              color: Colors.black,
                             ),
                           ),
                         );
                       },
                     ),
                   );
+                } else if (snapshot.hasError) {
+                  return Text('Error: ${snapshot.error}');
+                } else {
+                  final challenges = snapshot.data!;
+                  return GridChallenge(challenges: challenges);
                 }
               },
             ),
           ),
         ],
+      ),
+    );
+  }
+}
+
+class GridChallenge extends StatefulWidget {
+  const GridChallenge({super.key, required this.challenges});
+
+  final List challenges;
+
+  @override
+  GridChallengeState createState() => GridChallengeState();
+}
+
+class GridChallengeState extends State<GridChallenge> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  void _toggleTileExpansion(int index) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        final challenge = widget.challenges[index];
+        return Dialog(
+          backgroundColor: const Color.fromARGB(255, 153, 209, 92),
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.black),
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    child: Image.network(challenge['img']),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(
+                    challenge['name']['fr'],
+                    overflow: TextOverflow.clip,
+                    style: const TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(8.0, 0, 8.0, 16.0),
+                  child: Text(
+                    challenge['description']['fr'],
+                    overflow: TextOverflow.clip,
+                    style: const TextStyle(fontSize: 20),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        );
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: GridView.builder(
+        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 150,
+          crossAxisSpacing: 8.0,
+          mainAxisSpacing: 8.0,
+        ),
+        itemCount: widget.challenges.length,
+        itemBuilder: (context, index) {
+          final challenge = widget.challenges[index];
+          return GestureDetector(
+            onTap: () => _toggleTileExpansion(index),
+            child: Card(
+              color: const Color.fromARGB(255, 153, 209, 92),
+              child: Column(
+                children: [
+                  Expanded(
+                    child: Image.network(challenge['img'], fit: BoxFit.contain),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(5.0),
+                    child: FittedBox(
+                      child: Text(
+                        challenge['name']['fr'],
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          );
+        },
       ),
     );
   }
